193d35b134132448ded19079259a3bbb5c7b63f9
sushilmate <sushil.mate@outlook.com>
2019-06-30 09:52:09 +0530

    Adding the empty ConsoleUI project into solution.

---
+++ OnTheBeachJob.ConsoleUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OnTheBeachJob.ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
